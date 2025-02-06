ce0afc21344755771f8efa624d2b3fde082edb20
Jaime Lemos <147207213+JLemos03@users.noreply.github.com>
2025-02-05 22:36:30 -0500

    Add files via upload
    Agrego porcentajes y borrar gastos

--- ControlGastos/src/main/java/ultimoTrabajosemestre.java
+++ ControlGastos/src/main/java/ultimoTrabajosemestre.java
@@ -8,7 +8,7 @@ public class ultimoTrabajosemestre extends JFrame {
     private int saldoInicial;
     private JLabel saldoLabel;
     private JTextField montoField;
-    private JButton agregarButton, gastarButton;
+    private JButton agregarButton, gastarButton, borrarButton;
     private JProgressBar alimentacionBar, ropaBar, facturasBar, mascotasBar, transporteBar, gastosHormigaBar;
     private JComboBox<String> categoriaBox;
     private HashMap<String, JProgressBar> barrasGastos;
@@ -34,15 +34,19 @@ public class ultimoTrabajosemestre extends JFrame {
 
         
         agregarButton = new JButton("Agregar Dinero");
-        agregarButton.setBounds(50, 100, 150, 30);
+        agregarButton.setBounds(50, 100, 120, 30);
         add(agregarButton);
 
         
         gastarButton = new JButton("Registrar Gasto");
-        gastarButton.setBounds(220, 100, 150, 30);
+        gastarButton.setBounds(200, 100, 120, 30);
         add(gastarButton);
-
-        //agregado
+        
+        borrarButton = new JButton("Borrar Gasto");
+        borrarButton.setBounds(350, 100, 120, 30);
+        add(borrarButton);
+        
+        //agregado colision 
         categoriaBox = new JComboBox<>(new String[]{"Alimentación", "Ropa", "Facturas", "Mascotas", "Transporte", "Gastos Hormiga"});
         categoriaBox.setBounds(250, 60, 200, 30);
         add(categoriaBox);
@@ -62,6 +66,12 @@ public class ultimoTrabajosemestre extends JFrame {
             transporteBar = new JProgressBar(0, 100),
             gastosHormigaBar = new JProgressBar(0, 100)
         };
+        
+        //Configurar barra para mostrar %
+        for(JProgressBar barra: barras){
+            barra.setStringPainted(true);
+            barra.setString(barra.getValue() + "%");
+        }
 
         
         // agregado 
@@ -79,6 +89,7 @@ public class ultimoTrabajosemestre extends JFrame {
             barrasGastos.put(categorias[i], barras[i]);
         }
         
+        
 //         int j = ((i*100) / (int) saldo);
 //            System.out.println("AQUIIII" + j);
 
@@ -89,12 +100,19 @@ public class ultimoTrabajosemestre extends JFrame {
                     double monto = Double.parseDouble(montoField.getText());
                     
                     if (saldo == 0) { // Solo asignar saldoInicial la primera vez
-                         saldoInicial = (int) monto;
-                         
-                         saldo += monto;
+                        saldoInicial = (int) monto;
+                        saldo += monto;
                         saldoLabel.setText("Saldo: $" + saldo);
                         montoField.setText("");
-                    } else {
+                    } else if (saldo > 0) {
+                        saldoInicial += (int) monto;
+                        System.out.println(saldoInicial);
+                        saldo += monto;
+                        saldoLabel.setText("Saldo: $" + saldo);
+                        montoField.setText("");
+
+                    }
+                    else {
                        JOptionPane.showMessageDialog(null, "Ya ingresaste un saldo inicial");
 
                     }
@@ -130,9 +148,10 @@ public class ultimoTrabajosemestre extends JFrame {
 
                     JProgressBar barra = barrasGastos.get(categoria);
                     System.out.println(barra.getValue() );
-                    //int nuevoValor = Math.min(barra.getValue() + (int) monto, 100);
-                    int nuevoValor = ((int) monto * 100) / (int)saldoInicial ;
+                    int porcentajeGasto = (int) ((monto / saldoInicial)*100);
+                    int nuevoValor = Math.min(barra.getValue() + porcentajeGasto, 100); // Acumulado, sin pasarse del 100%
                     barra.setValue(nuevoValor);
+                    barra.setString(nuevoValor + "%");
                     System.out.println(nuevoValor);
 
                     montoField.setText("");
@@ -142,6 +161,47 @@ public class ultimoTrabajosemestre extends JFrame {
                 }
             }
         });
+        
+        borrarButton.addActionListener( new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                try {
+                    double monto = Double.parseDouble(montoField.getText());
+                    String categoria = (String) categoriaBox.getSelectedItem();
+                
+                    if (monto <= 0) {
+                        JOptionPane.showMessageDialog(null, "Ingrese un monto mayor a 0");
+                        return;
+                    }
+                    
+                    if ((monto+saldo) <= saldoInicial){
+                        saldo += monto;
+    
+                        saldoLabel.setText("Saldo: $" + saldo);
+
+                        JProgressBar barra = barrasGastos.get(categoria);
+                        System.out.println(barra.getValue() );
+
+                        if (monto > 0 && barra.getValue() >= (int) ((monto / saldoInicial) * 100)){
+                        int porcentajeGasto = (int) ((monto / saldoInicial)*100);
+                        // nno baja de cero
+                        int nuevoValor = Math.max(barra.getValue() - porcentajeGasto, 0); 
+                        barra.setValue(nuevoValor);
+                        barra.setString(nuevoValor + "%");
+                        System.out.println(nuevoValor);
+
+                        }
+
+                    
+                    }
+                    
+                    montoField.setText("");
+    
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(null, "Ingrese un número válido");
+                }
+            }
+        });
+        
         
     }
 
